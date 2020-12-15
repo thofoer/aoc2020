2027e41e0b85dc8f6c15702fd30fdff853f61c57
Thomas Foerster <thofoer@gmx.de>
2020-12-15 10:55:50 +0100

    day15

---
+++ day15_1.groovy
@@ -0,0 +1,24 @@
+input = "0,1,5,10,3,12,19"
+max = 2020 // 30000000 
+lastIndex =null
+list = input.split(",").collect { it as int }
+ix = 1
+indexMap = list.stream().inject([:]) { a,e -> a.put(e, ix); ix++; a }
+
+(list.size()..<max).stream().forEach {
+
+
+   last = list[it-1]
+   sz = indexMap[last]
+if (lastIndex!=null) {indexMap[lastIndex] = it}
+   if (sz==null || sz==it) {
+       list[it] = 0
+        lastIndex=0
+    }
+   else {
+       list[it] = it-sz
+       lastIndex=it-sz
+   }
+}
+
+println list[max-1]
