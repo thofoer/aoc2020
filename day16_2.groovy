ba830772e397da40313d814848662be5c13e672c
Thomas Foerster <thofoer@gmx.de>
2020-12-16 17:09:44 +0100

    day16

--- day16_2.groovy
+++ day16_2.groovy
@@ -1,9 +1,9 @@
-input="""departure location: 47-164 or 179-960
-departure station: 49-808 or 833-968
-departure platform: 50-561 or 567-964
-departure track: 29-407 or 416-954
-departure date: 48-317 or 339-965
-departure time: 42-366 or 390-952
+input="""departurelocation: 47-164 or 179-960
+departurestation: 49-808 or 833-968
+departureplatform: 50-561 or 567-964
+departuretrack: 29-407 or 416-954
+departuredate: 48-317 or 339-965
+departuretime: 42-366 or 390-952
 arrival location: 45-292 or 304-974
 arrival station: 26-255 or 266-951
 arrival platform: 47-225 or 243-957
@@ -267,12 +267,11 @@ data = input.split("\n\n")
 
 rules = data[0].split("\n").stream().collect { (it =~ "(.*): (\\d+)-(\\d+) or (\\d+)-(\\d+)")[0][1..5] }.collect { [ it[0], [it[1] as int, it[2] as int], [it[3] as int, it[4] as int] ]}
 
-
 def checkTicket(value) {
    rules.stream().noneMatch{ (it[1][0]<=value && it[1][1]>=value) || (it[2][0]<=value && it[2][1]>=value) }
 }
 
-
+myticket= data[1].split("\n")[1].split(",").collect { it as int }
 tickets = data[2].split("\n")[1..-1].collect { it.split(",").collect{ it as int } }.stream().filter { !it.any{ checkTicket(it) } }.toList()
 
 def check(rule, value) {
@@ -284,16 +283,40 @@ def checkRule(rule, values) {
 //println("-----> ${rule}    ${values}    ${values.every { check(rule, it) }}")
     values.every { check(rule, it) }
 }
-
+ix=0
+rules= rules.stream().collect { [it[0], it[1], it[2], ix++ ] }.toList()
+println rules
 res = (0..<tickets[0].size()).collect{
    i = it
-print i
    values = tickets.collect { it[i] }
-   foundRule = rules.stream().filter {checkRule(it, values) }.toList() [0]
-rules.remove(foundRule)
-println "  ${foundRule}"
+   foundRule = rules.stream().filter {checkRule(it, values) }.toList()
    foundRule
 }
+ii=0
+println "#################################################"
+println res.forEach {print "${ii++} ${it.size()}\t";  it.sort().forEach { print "${it[0]}   "  }; println "" }
+println "#################################################"
 
-println res.stream().filter {it!=null}.collect{ it[0]}.forEach { println it }
+
+
+
+res.sort { a,b -> a.size() - b.size() }
+
+for(i=1; i<res.size(); i++) {
+    for(j=0; j<i; j++) {
+        res[i].remove(res[j][0])
+    }
+}
+res = res.collect { it[0] }
+res.forEach { println it }
+println "--------"
+z = 1L
+for(i=0; i<res.size(); i++) {
+   if (res[i][0].startsWith("dep")) {
+      z *= myticket[res[i][3]]
+println res[i]
+   }
+}
+println myticket
+println 1L * myticket[17]*  myticket[11] *  myticket[8] *  myticket[14] *  myticket[6] *  myticket[1]
 
